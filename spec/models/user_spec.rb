9394deaf82945072200087dd19a58db6f121f300
Peter Frank Csoor <csoorpeterfrank@gmail.com>
2021-11-22 19:40:37 +0100

    create user model validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  context "validation tests" do
-    it "ensures"
+RSpec.fdescribe User, type: :model do
+  context "User model" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:params) { { user: { email: user.email, password: password } } }
+
+    context "when user attributes are valid" do
+      let(:password) { user.password }
+
+      it 'should be ok' do
+        expect(user).to be_valid
+      end
+    end
+
+    context "when user password is too short" do
+      let(:password){ "12345" }
+
+      it 'should fail' do
+        expect(user.valid_password?(params[:user][:password])).to be_falsey
+      end
+    end
+
   end
 end

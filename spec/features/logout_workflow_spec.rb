ec08fc008d257c8fb41e0698f7fbce6ac9594e28
Peter Frank Csoor <csoorpeterfrank@gmail.com>
2021-11-24 17:03:20 +0100

    create log out workflow tests

---
+++ spec/features/logout_workflow_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.fdescribe "Logout workflow", type: :feature do
+  let(:user){ FactoryBot.create(:user) }
+
+  before do
+    login_as(user, scope: :user)
+    visit "/"
+  end
+
+  context "when user is logged in" do
+    it "should be able to log out" do
+      expect(page).to have_text("Log out")
+    end
+
+    it "can log out" do
+      click_on("Log out")
+      expect(page).to have_text("Signed out successfully.")
+    end
+  end
+
+end

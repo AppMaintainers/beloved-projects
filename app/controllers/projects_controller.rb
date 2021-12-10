5da3210e48066747a3cb7dab9a98cabe3ef94e30
Peter Csoor <csoorpeterfrank@gmail.com>
2021-12-10 11:36:54 +0100

    create project actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,16 +1,35 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
-    @project = current_user.projects.build()
+    #byebug
+    @project = current_user.projects.build(project_params)
+    @project.users << current_user
+    if @project.save
+      flash[:success] = "Project created successfully!"
+      redirect_to root_url
+    else
+      flash[:warning] = "Project could not be created for some reason."
+      redirect_to root_url
+    end
   end
 
   def new
+    @project = Project.new
+  end
+
+  def show
+    @project = Project.find_by(id: params[:id])
   end
 
   private
 
-  def correct_user
-    @project = current_user.projects.find_by(id: params[id])
-    redirect_to root if @project.nil?
-  end
+    def project_params
+      params.require(:project).permit(:title, :context, :status_id)
+    end
+
+    def correct_user
+      @project = current_user.projects.find_by(id: params[id])
+      redirect_to root if @project.nil?
+    end
 end

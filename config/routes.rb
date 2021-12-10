67b8f8da959f178778365192ecdf14809b35d550
Peter Csoor <csoorpeterfrank@gmail.com>
2021-12-10 12:07:20 +0100

    create user and prject resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :projects
+  match '/users/:id',     to: 'users#show',       via: 'get'
+  resources :users, :only =>[:show]
 end

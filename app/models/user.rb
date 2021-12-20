4dde9795ba645639aa906c39d37b174a26836b94
Peter Csoor <csoorpeterfrank@gmail.com>
2021-12-20 10:05:29 +0100

    attach default avatar to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :projects
+  has_one_attached :image
+  after_commit :add_default_avatar, on: %i[create update]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -7,4 +9,19 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: true
+
+  private
+  def add_default_avatar
+    unless image.attached?
+      image.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default_avatar.jpg'
+          )
+        ),
+        filename: 'default_avatar.jpg',
+        content_type: 'image/jpg'
+      )
+    end
+  end
 end

ea9cc939ae564f54ed71267a67a3d26fa248684f
Peter Csoor <csoorpeterfrank@gmail.com>
2022-01-06 14:06:34 +0100

    concretize variable name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:first_name, :last_name, :email, :password, :password_confirmation, :remember_me]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    permitted_attributes = [:first_name, :last_name, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: permitted_attributes
+    devise_parameter_sanitizer.permit :account_update, keys: permitted_attributes
 
   end
 end

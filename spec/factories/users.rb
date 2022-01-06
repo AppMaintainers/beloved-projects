42d43b0820872d97b3f900fb88ef323e876b081a
Peter Csoor <csoorpeterfrank@gmail.com>
2022-01-06 15:59:10 +0100

    gen user properties with Faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    first_name { "John" }
-    last_name { "Doe" }
-    email { "john@example.com" }
-    password { "foobar" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
   end
 end

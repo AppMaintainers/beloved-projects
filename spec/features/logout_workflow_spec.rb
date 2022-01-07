24ebfe7fbc0b30b9d74a8edaef41317a177add10
Peter Csoor <csoorpeterfrank@gmail.com>
2022-01-07 11:13:52 +0100

    remove duplicated test case

--- spec/features/logout_workflow_spec.rb
+++ spec/features/logout_workflow_spec.rb
@@ -9,10 +9,6 @@ RSpec.describe "Logout workflow", type: :feature do
   end
 
   context "when user is logged in" do
-    it "should be able to log out" do
-      expect(page).to have_text("Log out")
-    end
-
     it "can log out" do
       click_on("Log out")
       expect(page).to have_text("Signed out successfully.")

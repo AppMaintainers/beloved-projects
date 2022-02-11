2930fe80b95cea6da5df8182aa8b3ab179763b0f
Peter Csoor <csoorpeterfrank@gmail.com>
2022-02-11 11:51:24 +0100

    Remove email validation in order to use devise's instead

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,4 @@ class User < ApplicationRecord
   has_and_belongs_to_many :projects
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: true
 end

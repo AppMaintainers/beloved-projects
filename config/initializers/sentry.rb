285a93787938cc4bcac85e1bbc4b1275558fb41a
Peter Csoor <csoorpeterfrank@gmail.com>
2022-02-17 16:17:00 +0100

    Resolve changing requests
    Set the proper DSN value and take out sample rating from sentry initialization file.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,10 +1,10 @@
 Sentry.init do |config|
-  config.dsn = 'https://examplePublicKey@o0.ingest.sentry.io/0'
+  config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # To activate performance monitoring, set one of these options.
   # We recommend adjusting the value in production:
-  config.traces_sample_rate = 0.5
+  # config.traces_sample_rate = 0.5
   # or
   config.traces_sampler = lambda do |context|
     true

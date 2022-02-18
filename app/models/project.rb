69c07ef3a52a82bfacd1a0a1cbc6d3f2ff282fd8
Peter Csoor <csoorpeterfrank@gmail.com>
2022-02-18 12:31:19 +0100

    Require status when creating projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
   has_and_belongs_to_many :users
   belongs_to :status, optional: true
+
+  validates :status, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,5 +22,5 @@ users = User.order(:created_at).take(6)
 1.times do |index|
   title = "Awesome project #{index+1}"
   context = Faker::Lorem.sentence(word_count: 5)
-  users.each { |user| user.projects.create!(title: title, context: context, status_id: Status.last) }
+  users.each { |user| user.projects.create!(title: title, context: context, status: Status.last) }
 end

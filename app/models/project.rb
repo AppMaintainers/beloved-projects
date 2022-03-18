0ac4c6d1ae3a2da269ac8f476f34308940312ea9
Peter Csoor <csoorpeterfrank@gmail.com>
2022-03-18 14:44:23 +0100

    Resolve requested changes

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,6 @@ class Project < ApplicationRecord
   has_and_belongs_to_many :users
   belongs_to :status
 
-  validates :status, presence: true
   validates :title, presence: true
   validates :acquired_at, presence: true
 end

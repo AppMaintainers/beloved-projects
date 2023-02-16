9827dd3ba8051c07e27aa21c12a1e0ffbc59a4c9
viktorJMarton <martonviktor666@gmail.com>
2023-02-16 11:36:04 +0000

    Fixed seed styling, email template in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 5.times do |n|
   first_name  = Faker::Name.first_name
   last_name  = Faker::Name.last_name
-  email = "example_#{n+1}@beloved.org"
+  email = "example-#{n+1}@beloved.org"
   password = "password"
   User.create!(first_name:  first_name,
                last_name: last_name,

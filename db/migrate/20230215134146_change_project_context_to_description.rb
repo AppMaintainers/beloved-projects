fcf9ec533031d6dcd4dd797baa11b04084894ce3
viktorJMarton <martonviktor666@gmail.com>
2023-02-16 11:05:25 +0000

    removed comments, changed context to desc. in seed , removed self. from method definition.

--- db/migrate/20230215134146_change_project_context_to_description.rb
+++ db/migrate/20230215134146_change_project_context_to_description.rb
@@ -1,10 +1,9 @@
 class ChangeProjectContextToDescription < ActiveRecord::Migration[6.1]
-  def self.up
+  def up
     rename_column :projects, :context, :description
-    #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
   end
 
-  def self.down
+  def down
     rename_column :projects, :description, :context
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 5.times do |n|
   first_name  = Faker::Name.first_name
   last_name  = Faker::Name.last_name
-  email = "example-#{n+1}@beloved.org"
+  email = "example_#{n+1}@beloved.org"
   password = "password"
   User.create!(first_name:  first_name,
                last_name: last_name,
@@ -21,7 +21,7 @@ Status.create(title: "Unknown", background: "#b3a8a9")
 users = User.order(:created_at).take(6)
 1.times do |index|
   title = "Awesome project #{index+1}"
-  context = Faker::Lorem.sentence(word_count: 5)
+  description = Faker::Lorem.sentence(word_count: 5)
   acquired_at = Time.now
-  users.each { |user| user.projects.create!(title: title, context: context, status: Status.last, acquired_at: acquired_at) }
+  users.each { |user| user.projects.create!(title: title, description: description, status: Status.last, acquired_at: acquired_at) }
 end

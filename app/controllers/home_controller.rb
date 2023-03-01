b50eb309973caf379ecb5dfb9e45d06144db740f
viktorJMarton <martonviktor666@gmail.com>
2023-03-01 10:24:05 +0000

    fixed nil project error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @projects = if params[:all] == "true"
+    @projects = if params[:all] == "true" 
       Project.all
     else
-      current_user.projects
+      current_user.projects unless current_user.nil?
     end
   end
 end

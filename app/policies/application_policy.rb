4db51abee6d3380f3e266ca8658f3133b1cb859e
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-06-20 15:51:27 +0200

    rubocop fixes

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -5,7 +5,8 @@ class ApplicationPolicy
   attr_reader :record
 
   def initialize(user, record)
-    raise Pundit::NotAuthorizedError, "must be logged in" unless user
+    raise Pundit::NotAuthorizedError, 'must be logged in' unless user
+
     @user   = user
     @record = record
   end
@@ -43,7 +44,8 @@ class ApplicationPolicy
     attr_reader :scope
 
     def initialize(user, scope)
-      raise Pundit::NotAuthorizedError, "must be logged in" unless user
+      raise Pundit::NotAuthorizedError, 'must be logged in' unless user
+
       @user  = user
       @scope = scope
     end

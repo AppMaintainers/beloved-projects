f3a9f6d24fd435da42ac7b0c32d4c2ea733d2fa7
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-06-22 10:39:49 +0200

    elsif cleared

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,8 @@ class HomeController < ApplicationController
   def index
     @projects = if params[:all] == "true"
                   policy_scope(Project.all)
-                elsif signed_in?
-                  policy_scope(current_user.projects)
                 else
-                  policy_scope(Project.none)
+                  policy_scope(current_user.projects)
                 end
   end
 end

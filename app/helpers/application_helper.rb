b7b3910cabc2db3541dbc79cac53473d1da373dc
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-07-03 12:57:21 +0200

    tosat message imlepmented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def bootstrap_class_for_toast(type)
+    case type
+      when 'alert'
+        'text-bg-danger'
+      when 'success'
+        'text-bg-success'
+      else
+        'text-bg-warning'
+    end
+
+  end
 end

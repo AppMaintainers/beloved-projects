d9c7a6dba3a2828a5cea7b13b5f99ca61bf04c89
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-07-05 10:06:56 +0200

    routing dismissed, requests fixed

--- app/controllers/maintainers_controller.rb
+++ app/controllers/maintainers_controller.rb
@@ -2,18 +2,28 @@
 
 class MaintainersController < ApplicationController
   before_action :load_project
+
   def edit
+    @users = policy_scope(User)
   end
 
   def create
     user = User.find_by(id: params[:maintainer_id])
-    @project.maintainers << user
+    if @project.maintainers << user
+      flash[:info] = 'Maintainer added successfully!'
+    else
+      flash[:danger] = 'Something went wrong!'
+    end
     redirect_to edit_project_maintainers_path
   end
 
   def destroy
     user = User.find_by(id: params[:maintainer_id])
-    @project.maintainers.destroy(user)
+    if @project.maintainers.destroy(user)
+      flash[:info] = 'Maintainer removed successfully!'
+    else
+      flash[:danger] = 'Something went wrong!'
+    end
     redirect_to edit_project_maintainers_path
   end
 

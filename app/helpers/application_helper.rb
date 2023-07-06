aba2e7d59eb01ba0f0378990b71750783317b3c0
zsuzsibelteky <zsuzsi@appmaintainers.com>
2023-07-06 12:32:27 +0200

    different color for different message types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,12 @@
 module ApplicationHelper
-
   def bootstrap_class_for_toast(type)
     case type
-      when 'alert'
-        'text-bg-danger'
-      when 'success'
-        'text-bg-success'
-      else
-        'text-bg-warning'
+    when 'alert'
+      'text-bg-danger'
+    when 'success', 'notice'
+      'text-bg-success'
+    else
+      'text-bg-warning'
     end
   end
 end

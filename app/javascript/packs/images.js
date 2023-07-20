431fb20c7b2b67758cbca2b96bf44822a422039f
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-07-20 12:01:41 +0200

    styling the project cards

--- app/javascript/packs/images.js
+++ app/javascript/packs/images.js
@@ -1,2 +1,3 @@
 require.resolve('../../../node_modules/@tabler/icons/icons/home.svg');
 require.resolve('../../../node_modules/@tabler/icons/icons/lifebuoy.svg');
+require.resolve('../../../node_modules/@tabler/icons/icons/plus.svg');

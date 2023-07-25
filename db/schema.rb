66617080d6d7fdc8cd6ae32d1e2f117b31928647
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-07-25 14:12:15 +0200

    remove related code to description and aquired_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_14_082557) do
+ActiveRecord::Schema.define(version: 2023_07_25_091259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,8 @@ ActiveRecord::Schema.define(version: 2023_07_14_082557) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
-    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "acquired_at"
   end
 
   create_table "projects_users", id: false, force: :cascade do |t|

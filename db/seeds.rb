af801a7e3e0282ed6c3d50c6da5afb363730977d
ViktorJMarton <viktor@appmaintainers.hu>
2023-07-27 12:37:08 +0200

    UselessTimes: Useless call to 1.times modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,9 +20,7 @@ Status.create(title: 'Unknown', background: '#b3a8a9')
 
 # Generate microposts for a subset of users.
 users = User.order(:created_at).take(6)
-1.times do |index|
-  title = "Awesome project #{index + 1}"
-  description = Faker::Lorem.sentence(word_count: 5)
-  acquired_at = Time.zone.now
-  users.each { |user| user.projects.create!(title: title, description: description, status: Status.last, acquired_at: acquired_at) }
-end
+title = "Awesome project #{0 + 1}"
+description = Faker::Lorem.sentence(word_count: 5)
+acquired_at = Time.zone.now
+users.each { |user| user.projects.create!(title: title, description: description, status: Status.last, acquired_at: acquired_at) }

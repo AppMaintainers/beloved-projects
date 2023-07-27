7e1ed116a6845fd925432d90085dc57865d40ae5
ViktorJMarton <viktor@appmaintainers.hu>
2023-07-27 06:17:11 +0200

    FrozenStringLiteral;EmptyLineAfterMagicComment

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Sentry.init do |config|
   config.dsn = ENV.fetch('SENTRY_DSN', nil)
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 5.times do |n|
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name

665daf2c01e692df17db445551f253ebd338ea2c
ViktorJMarton <viktor@appmaintainers.hu>
2023-07-27 06:41:16 +0200

    FrozenStringLiteral;EmptyLineAfterMagicComment

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Project < ApplicationRecord
   has_and_belongs_to_many :maintainers, class_name: 'User', join_table: 'projects_users'
   has_many :accounts, dependent: :destroy
--- db/migrate/20230215134146_change_project_context_to_description.rb
+++ db/migrate/20230215134146_change_project_context_to_description.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangeProjectContextToDescription < ActiveRecord::Migration[6.1]
   def up
     rename_column :projects, :context, :description

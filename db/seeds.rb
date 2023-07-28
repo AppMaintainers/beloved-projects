3a34ac47f0d23aa2c08ae18b2ce3e0ad668cb14a
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-07-28 14:41:31 +0200

    cleen up seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,4 @@
-5.times do |n|
-  first_name  = Faker::Name.first_name
-  last_name  = Faker::Name.last_name
-  email = "example-#{n+1}@beloved.org"
-  password = "password"
-  User.create!(first_name:  first_name,
-               last_name: last_name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
+# frozen_string_literal: true
 
-# Generate microposts for a subset of users.
-users = User.order(:created_at).take(6)
-1.times do |index|
-  title = "Awesome project #{index+1}"
-  users.each { |user| user.projects.create!(title: title, description: description, status: Status.last, acquired_at: acquired_at) }
-end
+FactoryBot.create_list(:user, 5)
+FactoryBot.create_list(:project, 3, maintainers: User.all)

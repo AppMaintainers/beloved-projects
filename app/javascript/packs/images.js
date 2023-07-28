364a86116ab62438e1bbbaaac961947ac0342691
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-07-28 11:44:25 +0200

    missing eye icon fixed

--- app/javascript/packs/images.js
+++ app/javascript/packs/images.js
@@ -1,3 +1,4 @@
+require.resolve('../../../node_modules/@tabler/icons/icons/eye.svg');
 require.resolve('../../../node_modules/@tabler/icons/icons/home.svg');
 require.resolve('../../../node_modules/@tabler/icons/icons/lifebuoy.svg');
 require.resolve('../../../node_modules/@tabler/icons/icons/plus.svg');

38961a3c3434c4c2a5dd1c73c2fbc1680a10de15
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-08-16 10:16:29 +0200

    validation added to accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,9 @@
 class Account < ApplicationRecord
   belongs_to :project
   belongs_to :account_manager, class_name: 'User'
+
+  validates :name, presence: true
+  validates :services, presence: true, allow_nil: false
+  validates :account_manager_id, allow_nil: false
+  validates :owner_request, presence: true, allow_nil: false
 end

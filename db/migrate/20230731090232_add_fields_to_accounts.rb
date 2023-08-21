7eab67799efe400078c2a284d2898129f5539507
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-08-21 12:02:49 +0200

    fixing migration

--- db/migrate/20230731090232_add_fields_to_accounts.rb
+++ db/migrate/20230731090232_add_fields_to_accounts.rb
@@ -5,7 +5,7 @@ class AddFieldsToAccounts < ActiveRecord::Migration[6.1]
     change_table :accounts, bulk: true do |t|
       t.string :domain
       t.string :organization
-      t.jsonb  :services, default: []
+      t.jsonb  :services, null: false, default: []
       t.references :account_manager, foreign_key: { to_table: :users }
       t.boolean :owner_request, null: false, default: false
       t.text :owner_request_notes

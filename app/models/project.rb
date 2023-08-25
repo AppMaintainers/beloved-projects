a1c3d05d65942f5e3bda70136e7acc3e92fdb628
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-08-25 13:13:51 +0200

    add modell

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
   has_and_belongs_to_many :maintainers, class_name: 'User', join_table: 'projects_users'
   has_many :accounts, dependent: :destroy
+  has_many :links, dependent: :destroy
 
   validates :title, presence: true
 end

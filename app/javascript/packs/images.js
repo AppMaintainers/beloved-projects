4109c2ffa24d7330747e31654be50f7978e9377e
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-09-04 14:44:40 +0200

    add delete button

--- app/javascript/packs/images.js
+++ app/javascript/packs/images.js
@@ -3,3 +3,4 @@ require.resolve('../../../node_modules/@tabler/icons/icons/home.svg');
 require.resolve('../../../node_modules/@tabler/icons/icons/lifebuoy.svg');
 require.resolve('../../../node_modules/@tabler/icons/icons/link.svg');
 require.resolve('../../../node_modules/@tabler/icons/icons/plus.svg');
+require.resolve('../../../node_modules/@tabler/icons/icons/trash.svg');

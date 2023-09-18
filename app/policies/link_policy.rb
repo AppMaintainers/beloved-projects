4c23b0538bb0a6051b30de8078af94a27d8f688c
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-09-18 16:51:16 +0200

    add destroy action and policy

--- app/policies/link_policy.rb
+++ app/policies/link_policy.rb
@@ -13,6 +13,10 @@ class LinkPolicy < ApplicationPolicy
     record.project.maintainers.include?(user)
   end
 
+  def destroy?
+    record.project.maintainers.include?(user)
+  end
+
   class Scope < Scope
     def resolve
       scope.all

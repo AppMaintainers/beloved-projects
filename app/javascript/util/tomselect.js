9c4265e0c3b4377f225892f3dbbdb09b368a8583
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-09-26 14:46:27 +0200

    modify tomselect

--- app/javascript/util/tomselect.js
+++ app/javascript/util/tomselect.js
@@ -1,5 +1,7 @@
 const TomSelect = require('tom-select');
 
 if (document.querySelector('[data-tomselect]')) {
-    new TomSelect('[data-tomselect]', { create: true });
+    document.querySelectorAll('[data-tomselect]').forEach((el) => {
+        new TomSelect(`#${el.id}`, JSON.parse(el.dataset.tomselectOptions));
+    });
 }

28d4884dffac6cd18a0e2bbc7e0677e368b7c1d0
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-10-24 16:03:34 +0200

    rubocop fixes

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,24 +1,15 @@
 # frozen_string_literal: true
 
 class AdminsController < ApplicationController
-
   def create
     @user = authorize User.find_by(id: params[:user_id]), :edit?
-    if policy([:attributes, @user]).valid_admin?(true)
-      @user.update(admin: true)
-    else
-      raise Pundit::NotAuthorizedError
-    end
+    (raise Pundit::NotAuthorizedError unless policy([:attributes, @user]).valid_admin?(true))
     redirect_to users_path
   end
 
   def destroy
     @user = authorize User.find_by(id: params[:user_id]), :edit?
-    if policy([:attributes, @user]).valid_admin?(false)
-      @user.update(admin: false)
-    else
-      raise Pundit::NotAuthorizedError
-    end
+    (raise Pundit::NotAuthorizedError unless policy([:attributes, @user]).valid_admin?(false))
     redirect_to users_path
   end
 end

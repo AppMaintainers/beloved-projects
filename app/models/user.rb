ff39c16cf88edf5cf3e5d1e87fdf1371cd16fd74
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-10-25 16:11:02 +0200

    admin validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   has_and_belongs_to_many :projects
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :admin, inclusion: { in: [true, false] }
 end

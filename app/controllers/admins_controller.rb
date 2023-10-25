5d1296b0659ba50cd6375c9527030c898c102502
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-10-25 16:33:13 +0200

    admin management fixed

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,13 +3,21 @@
 class AdminsController < ApplicationController
   def create
     @user = authorize User.find_by(id: params[:user_id]), :edit?
-    (raise Pundit::NotAuthorizedError unless policy([:attributes, @user]).valid_admin?(true))
+    if policy([:attributes, @user]).valid_admin?(true)
+      @user.update(admin: true)
+    else
+      raise Pundit::NotAuthorizedError
+    end
     redirect_to users_path
   end
 
   def destroy
     @user = authorize User.find_by(id: params[:user_id]), :edit?
-    (raise Pundit::NotAuthorizedError unless policy([:attributes, @user]).valid_admin?(false))
+    if policy([:attributes, @user]).valid_admin?(false)
+      @user.update(admin: false)
+    else
+      raise Pundit::NotAuthorizedError
+    end
     redirect_to users_path
   end
 end

f94a1ecebb158cebd5e6cbacf82cc6ce39a194df
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-12-18 13:53:57 +0100

    show action removed from user policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class UserPolicy < ApplicationPolicy
-  def show?
-    true
-  end
-
   def new?
     admin?
   end
--- spec/policies/admin/user_policy_spec.rb
+++ spec/policies/admin/user_policy_spec.rb
@@ -5,17 +5,11 @@ require 'rails_helper'
 RSpec.describe UserPolicy do
   let(:user) { create(:user, :admin) }
 
-  describe 'how to handle general use cases' do
-    let(:object_to_check) { User }
-
-    it { is_expected.to permit_actions [:show] }
-  end
-
   describe 'how to handle concrete objects' do
     let(:object_to_check) { User }
     let(:checked_user) { create(:user) }
 
-    it { is_expected.to permit_actions [:new, :create, :show, :edit, :update, :index] }
-    it { is_expected.to forbid_actions [:destroy] }
+    it { is_expected.to permit_actions [:new, :create, :edit, :update, :index] }
+    it { is_expected.to forbid_actions [:show, :destroy] }
   end
 end

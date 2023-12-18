182fc3efbe948284b7cb0bc8002f7c8e61f560ca
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2023-12-18 14:21:08 +0100

    added filter to user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,8 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :admin)
+    unfiltered_user_params = params.require(:user).permit(:first_name, :last_name, :email, :admin)
+    policy([:attributes, :user]).filter(unfiltered_user_params)
   end
 
   def load_user

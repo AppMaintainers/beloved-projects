1e03b6f33684a09ca2d3aadca75ae092281c8c48
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2024-01-25 16:29:41 +0100

    admins can deactivate basic users at admin page

--- app/controllers/deactivates_controller.rb
+++ app/controllers/deactivates_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class DeactivatesController < ApplicationController
-
   def destroy
     @user = authorize User.find_by(id: params[:user_id]), :edit?
-    if !@user.admin?
-      @user.update(deactivated_at: DateTime.now)
-    else
+    if @user.admin?
       raise Pundit::NotAuthorizedError
+    else
+      @user.update(deactivated_at: DateTime.now)
     end
+
     redirect_to users_path
   end
 end

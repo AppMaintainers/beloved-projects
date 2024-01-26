a243d95d5cffbec59075f5af309e65093c61ee3a
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2024-01-26 11:50:44 +0100

    error handeling and policy checks added to deactivating users

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -4,10 +4,13 @@ module Auth
   class RegistrationsController < Devise::RegistrationsController
     # The default destroy is changed to only deactivate the user, based on the official devise github
     def destroy
-      resource.update(deactivated_at: DateTime.now) if resource.deactivated_at.present?
+      resource.update!(deactivated_at: DateTime.now) if resource.deactivated_at.blank?
       sign_out
       set_flash_message! :noticed, :destroyed
       respond_with_navigational(resource) { redirect_to after_sign_out_path_for(resource_name) }
+    rescue ActiveRecord::RecordInvalid
+      flash[:alert] = 'Your account cannot be deactivated.'
+      redirect_to edit_user_registration_path
     end
   end
 end

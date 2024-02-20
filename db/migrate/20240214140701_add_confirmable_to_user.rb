c0baeda3a0f1582496c49561cd89f75aa61695cb
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2024-02-20 10:55:39 +0100

    migration changed to one change method

--- db/migrate/20240214140701_add_confirmable_to_user.rb
+++ db/migrate/20240214140701_add_confirmable_to_user.rb
@@ -1,25 +1,15 @@
 # frozen_string_literal: true
 
 class AddConfirmableToUser < ActiveRecord::Migration[6.1]
-  def up
+  def change
     change_table :users, bulk: true do |t|
       t.string :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
       t.string :unconfirmed_email
+      t.index :confirmation_token, unique: true
     end
-    add_index :users, :confirmation_token, unique: true
 
     User.update(confirmed_at: DateTime.now)
-  end
-
-  def down
-    remove_index :users, :confirmation_token
-    change_table :users, bulk: true do |t|
-      t.string :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string :unconfirmed_email
-    end
   end
 end

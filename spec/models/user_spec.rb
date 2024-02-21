97ef5c9efdd937f46b81743a2a8f46493dfcf9f5
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2024-02-21 13:15:41 +0100

    RSpec/ExampleWithoutDescription fixed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,13 +17,13 @@ RSpec.describe User, type: :model do
     end
 
     context "when email address is valid" do
-      it {
+      it 'passes' do
         is_expected.to allow_value('user@example.com',
                                    'USER@foo.COM',
                                    'A_US-ER@foo.bar.org',
                                    'first.last@foo.jp',
                                    'alice+bob@baz.cn').for(:email)
-      }
+      end
     end
 
     context "when email address is not valid" do

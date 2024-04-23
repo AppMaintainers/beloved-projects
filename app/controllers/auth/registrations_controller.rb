35fc77d242ffa075696829338d7485e207f7b84f
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2024-04-23 12:23:48 +0200

    comment added, update hashes mereged

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -2,13 +2,15 @@
 
 module Auth
   class RegistrationsController < Devise::RegistrationsController
+    # The default update is completed with the auto-generated password's case.
+    # If the user never changed the auto-generated password before,
+    # it does not need to know it for the first password change.
     def update
       self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
       prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
 
       resource_updated = if resource.auto_gen_password
-                           resource.update(account_update_params)
-                           resource.update(auto_gen_password: false)
+                           resource.update(account_update_params.merge(auto_gen_password: false))
                          else
                            update_resource(resource, account_update_params)
                          end
@@ -29,7 +31,7 @@ module Auth
     # The default destroy is changed to only deactivate the user, based on the official devise github
     def destroy
       resource.update!(deactivated_at: DateTime.now) if resource.deactivated_at.blank?
-      sign_outli
+      sign_out
       set_flash_message! :notice, :destroyed
       respond_with_navigational(resource) { redirect_to after_sign_out_path_for(resource_name) }
     rescue ActiveRecord::RecordInvalid

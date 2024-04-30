784de80680e25b9c25035937a6aac08bc531ac57
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2024-04-30 12:12:03 +0200

    Capybara/ClickLinkOrButtonStyle fixed

--- spec/features/password_change_spec.rb
+++ spec/features/password_change_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'Password change' do
     let(:user) { create(:user, auto_gen_password: true) }
 
     it 'can change the password' do
-      click_button('Update')
+      click_on('Update')
       expect(page).to have_text('Your account has been updated successfully.')
     end
   end
@@ -25,14 +25,14 @@ RSpec.describe 'Password change' do
   context 'with giving the current password' do
     it 'can change the password' do
       fill_in('user[current_password]', with: user.password)
-      click_button('Update')
+      click_on('Update')
       expect(page).to have_text('Your account has been updated successfully.')
     end
   end
 
   context 'without giving the current password' do
     it 'can not change the password' do
-      click_button('Update')
+      click_on('Update')
       expect(page).to have_text('Current password can\'t be blank')
     end
   end

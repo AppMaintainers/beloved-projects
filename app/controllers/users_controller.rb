4346c53eff7f2203dab3fe6cc41d8751991f88c4
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2024-05-09 11:31:14 +0200

    devise error messages styled as flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       flash[:alert] = @user.errors.full_messages.join('. ')
-      redirect_to edit_users_path(@user)
+      redirect_to edit_user_path(@user)
     end
   end
 

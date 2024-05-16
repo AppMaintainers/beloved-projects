15b0e2d2b476fb3ea3a937138b5b92ac58f4ec7e
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2024-05-16 15:52:25 +0200

    add deactivated_at to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_18_085037) do
+ActiveRecord::Schema.define(version: 2024_05_16_094736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2024_04_18_085037) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deactivated_at"
   end
 
   create_table "projects_users", id: false, force: :cascade do |t|

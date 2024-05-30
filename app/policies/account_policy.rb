0110dce3c3adcf63a5f67212047489cf5d98b59b
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2024-05-30 16:13:08 +0200

    account policy updated with deactivated project and tests added

--- app/policies/account_policy.rb
+++ app/policies/account_policy.rb
@@ -2,19 +2,19 @@
 
 class AccountPolicy < ApplicationPolicy
   def new?
-    true
+    record.project.deactivated_at.nil?
   end
 
   def create?
-    admin? || record.project.maintainers.include?(user)
+    (admin? || record.project.maintainers.include?(user)) && record.project.deactivated_at.nil?
   end
 
   def update?
-    admin? || record.project.maintainers.include?(user)
+    (admin? || record.project.maintainers.include?(user)) && record.project.deactivated_at.nil?
   end
 
   def destroy?
-    admin? || record.project.maintainers.include?(user)
+    (admin? || record.project.maintainers.include?(user)) && record.project.deactivated_at.nil?
   end
 
   class Scope < Scope

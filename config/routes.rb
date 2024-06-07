e65abcda1ad54ebd1fde25cd123a58950a45dd86
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2024-06-07 11:29:22 +0200

    project route restrictions added

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, path: 'auth', controllers: { registrations: 'auth/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :projects do
+  resources :projects, only: [:show, :new, :create, :edit, :update, :destroy] do
     resources :accounts, only: [:new, :create, :index, :edit, :update, :destroy]
     resource :maintainers, only: [:edit]
     resources :maintainers, only: [] do

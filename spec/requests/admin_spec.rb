714b179b6776af51e9821863230805d36edaa618
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2024-06-18 15:22:58 +0200

    admin action tests added

---
+++ spec/requests/admin_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Admin' do
+  let(:admin) { create(:user, :admin) }
+
+  before { login_as admin }
+
+  context 'when user added as admin' do
+    let(:user) { create(:user) }
+
+    it 'user becomes admin' do
+      expect { post user_admin_path(user) }.to change { user.reload.admin }.from(false).to(true)
+    end
+
+    it 'redirects correctly' do
+      post user_admin_path(user)
+
+      expect(response).to redirect_to(users_path)
+    end
+  end
+
+  context 'when user removed as admin' do
+    it 'user is no longer admin' do
+      expect { delete user_admin_path(admin) }.to change { admin.reload.admin }.from(true).to(false)
+    end
+
+    it 'redirects correctly' do
+      delete user_admin_path(admin)
+
+      expect(response).to redirect_to(users_path)
+    end
+  end
+end

2e9642b394fe06c295e09fb6af2a52a769a2be1d
zsuzsibelteky <zsuzsi.belteky@gmail.com>
2024-07-11 14:46:46 +0200

    project workflow test added

---
+++ spec/features/project_workflow_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# rubocop:disable RSpec/ExampleLength, RSpec/MultipleExpectations
+
+RSpec.describe 'Create project' do
+  let(:user) { create(:user, :admin) }
+
+  before { login_as user }
+
+  context 'with account and link' do
+    it 'is successful' do
+      visit '/'
+
+      click_on('Create new project')
+      fill_in('project[title]', with: 'Test project')
+      click_on('Save')
+      expect(page).to have_text('Project created successfully!')
+
+      click_on('Test project')
+      click_on('New')
+      click_on('Account')
+      fill_in('account[name]', with: 'Test account')
+      fill_in('account[services]', with: 'test')
+      select(user.first_name, from: 'account[account_manager_ids][]')
+      click_on('Save')
+      expect(page).to have_text('Account created successfully!')
+
+      click_on('New')
+      click_on('Link')
+      fill_in('link[title]', with: 'Test link')
+      fill_in('link[url]', with: 'http://test.com')
+      click_on('Save')
+      expect(page).to have_text('Link created successfully!')
+    end
+  end
+end
+
+# rubocop:enable RSpec/ExampleLength, RSpec/MultipleExpectations

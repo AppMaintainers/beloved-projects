0ee9baa58ea41be63a9fc35051dc03721f74e627
Gregurevic <paul.greg.indy@gmail.com>
2024-08-26 14:09:26 +0200

    amazon ses integration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,16 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  # Amazon SES (Simple Email Service) integration
+  config.x.mail_from = 'info@appmaintainers.com'
+  config.action_mailer.default_url_options = { host: 'https://beloved-projects.herokuapp.com/' }
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch('AMAZON_SES_ADDRESS', nil),
+    user_name: ENV.fetch('AMAZON_SES_USERNAME', nil),
+    password: ENV.fetch('AMAZON_SES_PASSWORD', nil)
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

d1e48e8c47000f6dccb1709b4625acf86ffd932e
Gregurevic <paul.greg.indy@gmail.com>
2024-09-09 11:21:07 +0200

    using authenticated in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, path: 'auth', controllers: { registrations: 'auth/registrations' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :projects, only: [:show, :new, :create, :edit, :update, :destroy] do
-    resources :accounts, only: [:new, :create, :index, :edit, :update, :destroy]
-    resource :maintainers, only: [:edit]
-    resources :maintainers, only: [] do
-      resource :connection, only: [:create, :destroy], controller: 'maintainers', path: ''
+
+  devise_for :users, path: 'auth', controllers: { registrations: 'auth/registrations' }
+
+  authenticated do
+    resources :projects, only: [:show, :new, :create, :edit, :update, :destroy] do
+      resources :accounts, only: [:new, :create, :index, :edit, :update, :destroy]
+      resource :maintainers, only: [:edit]
+      resources :maintainers, only: [] do
+        resource :connection, only: [:create, :destroy], controller: 'maintainers', path: ''
+      end
+      resources :links, only: [:new, :create, :index, :edit, :update, :destroy]
     end
-    resources :links, only: [:new, :create, :index, :edit, :update, :destroy]
+    resources :users, only: [:new, :create, :edit, :update, :index] do
+      resource :admin, only: [:create, :destroy]
+      resource :deactivate, only: [:destroy]
+    end
+    resource :magic_link, only: [:create, :show]
   end
-  resources :users, only: [:new, :create, :edit, :update, :index] do
-    resource :admin, only: [:create, :destroy]
-    resource :deactivate, only: [:destroy]
-  end
-  resource :magic_link, only: [:create, :show]
 end

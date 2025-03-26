e59f87c2d1dac659b080bd8445b12075481eb00e
Gregurevic <paul.greg.indy@gmail.com>
2025-03-26 13:48:50 +0100

    remove maintainers if deactivated

--- app/controllers/deactivates_controller.rb
+++ app/controllers/deactivates_controller.rb
@@ -2,20 +2,21 @@
 
 class DeactivatesController < ApplicationController
   def destroy
+    skip_authorization
+
     @user = User.find_by(id: params[:user_id])
+    deactivated_at = DateTime.now
 
-    if policy([:attributes, @user]).valid_deactivated_at?(DateTime.now)
-      skip_authorization
+    raise Pundit::NotAuthorizedError unless policy([:attributes, @user]).valid_deactivated_at?(deactivated_at)
 
-      if @user.update(deactivated_at: DateTime.now)
-        flash[:notice] = 'User deactivated successfully!'
-      else
-        flash[:alert] = 'This user cannot be deactivated.'
-      end
-
-      redirect_to users_path
-    else
-      raise Pundit::NotAuthorizedError
+    ApplicationRecord.transaction do
+      @user.update(deactivated_at: deactivated_at)
+      @user.projects_users.delete_all
     end
+
+    flash[:notice] = 'User deactivated successfully!'
+    redirect_to users_path
+  rescue ActiveRecord::RecordInvalid
+    flash[:alert] = 'This user cannot be deactivated.'
   end
 end
